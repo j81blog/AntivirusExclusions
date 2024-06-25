a6974945dd6e2754ce1e93f9a1c0739b1a8e0ed5
John B <john@j81.nl>
2024-06-25 15:38:25 +0200

    Add pdf download button

--- Set-AVEHTMLCode.ps1
+++ Set-AVEHTMLCode.ps1
@@ -13,18 +13,31 @@ $htmlCode.Append(@"
 <html lang="en">
 
 <head>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tabulator/6.2.1/css/tabulator_site.min.css"
-        integrity="sha512-9LQ1wW/0o26vV8KSgztjahv/AWL+6xR+pwsDQI9V7SLMINkszW9ZN5qmIrxFmZdktGTM9dHm225D4ilSbCw5zw=="
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tabulator/6.2.1/css/tabulator_bulma.min.css"
+        integrity="sha512-Ix1tDtTLrfGS60Xj232WgTr50m8srh4PO5DAwqhi8DI6gnw3/ewIVMpVAD06gSTND/V5yVysEQ/E6xImUUv+BQ=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/tabulator/6.2.1/js/tabulator.min.js"
         integrity="sha512-+FOZvgG76G8TTi7fxXsyGIA5XKmZXTOO2rLYtqakgs5ct1qsUSaO8/UP6Z3xbsyxL/hjS7mhYacG7CPmevI6Cw=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        crossorigin="anonymous" referrerpolicy="no-referrer">
+    </script>
 
     <!-- the line below is necessary for the Date sorter to work - it is buried in the v5.1 documentation here
     https://tabulator.info/docs/6.2/sort#func-builtin -->
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/luxon/3.4.4/luxon.min.js"
         integrity="sha512-dUlSLLkxslGILhPdCkALwk4szPhp3xmZIKFtlUD+O9Lslq41Aksmdt5OGqpomDoT4FsCUH70jQU8ezZHI3v1RQ=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        crossorigin="anonymous" referrerpolicy="no-referrer">
+    </script>
+    <!-- the two scripts below are necessary for the export to pdf to work - https://tabulator.info/docs/6.2/download#overview -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"
+        integrity="sha512-qZvrmS2ekKPF2mSznTQsxqPgnpkI4DNTlrdUmTzrDgektczlKNRRhy5X5AAOnx5S09ydFYWWNSfcEqDTTHgtNA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.8.2/jspdf.plugin.autotable.min.js"
+        integrity="sha512-2/YdOMV+YNpanLCF5MdQwaoFRVbTmrJ4u4EpqS/USXAQNUDgI5uwYi6J98WVtJKcfe1AbgerygzDFToxAlOGEQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer">
+    </script>
+
    <meta charset="utf-8" />
     <title>Anti Virus Exclusions</title>
 </head>
@@ -34,6 +47,26 @@ $htmlCode.Append(@"
         max-width: 80vw;
         background-color: #A3BFD1;
         margin: 0 auto;
+        font-family: 'Montserrat-optimized', Monserrat, Verdana, sans-serif;
+        font-size: 12px;
+    }
+    .tabulator {
+        font-size: 12px;
+    }
+    .button {
+        border: none;
+        color: white;
+        padding: 10px 15px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        margin: 4px 2px;
+        cursor: pointer;
+    }
+
+    .button1 {
+        background-color: #3759d7;
     }
 </style>
 
@@ -63,9 +96,15 @@ ForEach ($csvFile in $csvFiles) {
             </h2>
             <div id="$($shortCode)-table"></div>
             <script>
-                function $($shortCode)down() {
+                function $($shortCode)csvdown() {
                     $($shortCode)table.download("csv", "$($csvFilename)", {
-                        delimiter: ","
+                        delimiter: ",",
+                    });
+                }
+                function $($shortCode)pdfdown() {
+                    $($shortCode)table.download("pdf", "$("$($csvFilename)".replace("csv","pdf"))", {
+                        orientation:"landscape",
+                        title:"Av exclusions: $($title)",
                     });
                 }
                 var $($shortCode)table = new Tabulator("#$($shortCode)-table", {
@@ -81,7 +120,7 @@ ForEach ($csvFile in $csvFiles) {
                         { title: "Justification", field: "Justification", hozAlign: "left", formatter: "textarea" }
                     ],
 
-                    footerElement: "<button id='download-$($shortCode)-csv' onclick='$($shortCode)down();'>Download CSV</button>"
+                    footerElement: "<div><button id='download-$($shortCode)-csv' onclick='$($shortCode)csvdown();' class='button button1'>Download CSV</button><button id='download-$($shortCode)-pdf' onclick='$($shortCode)pdfdown();' class='button button1'>Download PDF</button></div>",
                 });
             </script>
             </div>
