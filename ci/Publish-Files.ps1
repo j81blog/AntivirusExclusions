455a587e7fca62b6dce2cbcd0d8631f12b4e9b81
John B <john@j81.nl>
2024-06-28 13:52:29 +0200

    chore: Update Publish-Files.ps1 to improve SSH session creation

--- ci/Publish-Files.ps1
+++ ci/Publish-Files.ps1
@@ -1,14 +1,20 @@
 [CmdletBinding()]
 [OutputType()]
-param ()
+param (
+    [string]$sshHost = "${env:SSHHost}",
 
-$SSHHost = "${env:SSHHost}"
-$Port = '22'
-$SSHUsername = "${env:SSHUsername}"
-$SSHPassword = "${env:SSHPassword}"
-$SSHPath = "${env:SSHPath}"
-$OutputPath = "output"
+    [int]$Port = 22,
 
+    [string]$SSHUsername = "${env:SSHUsername}",
+
+    [string]$SSHPassword = "${env:SSHPassword}",
+
+    [string]$sshPath = "${env:SSHPath}",
+
+    [string]$outputPath = "output"
+)
+
+$exitCode = 0
 
 if (-Not ([String]::IsNullOrEmpty("${env:GITHUB_WORKSPACE}")) -and (Test-Path -Path "${env:GITHUB_WORKSPACE}" -ErrorAction SilentlyContinue)) {
     Write-Host "GITHUB_WORKSPACE..: ${env:GITHUB_WORKSPACE}"
@@ -16,13 +22,30 @@ if (-Not ([String]::IsNullOrEmpty("${env:GITHUB_WORKSPACE}")) -and (Test-Path -P
     Write-Host "GITHUB Branch.....: ${env:GITHUB_REF_NAME}"
     $projectRoot = "${env:GITHUB_WORKSPACE}"
     $securePassword = ConvertTo-SecureString $SSHPassword -AsPlainText -Force
-    $Credential = New-Object System.Management.Automation.PSCredential ($SSHUsername, $securePassword)
+    $sshCredential = New-Object System.Management.Automation.PSCredential ($SSHUsername, $securePassword)
 } else {
     Write-Host "Local run"
     $projectRoot = ( Resolve-Path -Path ( Split-Path -Parent -Path $PSScriptRoot ) ).Path
-    $Credential = Get-Credential -Message "Enter SSH credentials"
-    $SSHHost = Read-Host -Prompt "Enter SSH Host"
-    $SSHPath = Read-Host -Prompt "Enter SSH Path"
+    if ($null -ne ${Env:SSHUsername} -and $null -ne ${Env:SSHPassword}) {
+        Write-Host "Using SSH credentials from environment"
+        $securePassword = ConvertTo-SecureString $SSHPassword -AsPlainText -Force
+        $sshCredential = New-Object System.Management.Automation.PSCredential ($SSHUsername, $securePassword)
+        Write-Host "Credentials set"
+    } else {
+        $sshCredential = Get-Credential -Message "Enter SSH credentials"
+    }
+    if ($null -ne ${Env:SSHHost}) {
+        Write-Host "Using SSH Host from environment"
+        $sshHost = ${Env:SSHHost}
+    } else {
+        $sshHost = Read-Host -Prompt "Enter SSH Host"
+    }
+    if ($null -ne ${Env:SSHPath}) {
+        Write-Host "Using SSH Path from environment"
+        $sshPath = ${Env:SSHPath}
+    } else {
+        $sshPath = Read-Host -Prompt "Enter SSH Path"
+    }
 }
 
 Write-Host "Project root......: $projectRoot"
@@ -43,7 +66,7 @@ if (Get-Module -Name Posh-SSH) {
     Import-Module -Name Posh-SSH
 }
 
-$FilesPath = [System.IO.Path]::Combine($projectRoot, $OutputPath)
+$FilesPath = [System.IO.Path]::Combine($projectRoot, $outputPath)
 
 Write-Host "Creating output directory and content"
 & "$PSSCriptRoot\Set-AVEHTMLCode.ps1" -ProjectRoot $projectRoot -OutputPath $FilesPath
@@ -52,35 +75,38 @@ Write-Host "Output directory and content created"
 if (Test-Path -Path $FilesPath) {
     $files = Get-ChildItem -Path $FilesPath
     Write-Host "$($files.Count) Files to copy, connecting to host"
-    $SSHSession = New-SFTPSession -ComputerName $SSHHost -Port $Port -Credential $Credential -Force
+    $sshSession = New-SFTPSession -ComputerName $sshHost -Port $Port -Credential $sshCredential -Force
 
-    if ($SSHSession.Connected -eq $true) {
+    if ($sshSession.Connected -eq $true) {
         Write-Host "Connected to host, copying files"
 
         ForEach ($file in $files) {
             Write-Host "Uploading file: $file"
-            Set-SFTPItem -Session $SSHSession.SessionID -Destination $SSHPath -Path $file.FullName -Force
+            Set-SFTPItem -Session $sshSession.SessionID -Destination $sshPath -Path $file.FullName -Force
         }
         Write-Host "Files copied"
-        try {
-            Write-Host "Trying to clean up files in $FilesPath"
-            Remove-Item -Path $FilesPath -Recurse -Force
-            Write-Host "Files cleaned up"
-        } catch {
-            Write-Warning "Failed to clean up files, $($_.Exception.Message)"
-        }
 
         Write-Host "Disconnecting"
-        if (Remove-SFTPSession -SFTPSession $SSHSession) {
+        if (Remove-SFTPSession -SFTPSession $sshSession) {
             Write-Host "Disconnected"
         } else {
             Write-Host "Failed to disconnect"
         }
     } else {
         Write-Host "Failed to connect to host"
-        Exit 1
+        $exitCode = 1
     }
 } else {
     Write-Host "No files to copy"
-    Exit 0
 }
+
+try {
+    Write-Host "Trying to clean up files in $FilesPath"
+    Remove-Item -Path $FilesPath -Recurse -Force
+    Write-Host "Files cleaned up"
+} catch {
+    Write-Warning "Failed to clean up files, $($_.Exception.Message)"
+}
+
+Write-Host "Exiting [$exitCode]"
+exit $exitCode
